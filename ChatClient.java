a65eb4221bd1b432bcb00b2e5034b2fed5d25c5e
Kayla Ziegler <ziegl107@mail.chapman.edu>
2016-04-14 19:35:24 -0700

    test mult-user chat files

---
+++ ChatClient.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.net.*;
+
+public class ChatClient {
+	
+	public static void main(String[] args) {
+
+		MulticastSocket socket = null;
+		DatagramPacket packet = null;
+
+		byte[] buffer = new byte[256];
+
+		try {
+			socket = new MulticastSocket(6789);
+			InetAddress address = InetAddress.getByName("localhost");
+			socket.joinGroup(address);
+
+
+			while(true) {
+				packet = new DatagramPacket(buffer, buffer.length);
+				socket.receive(packet);
+				String printOut = new String(buffer, 0, packet.getLength());
+				System.out.println(packet.getAddress+ ": "+ printOut);
+			}
+		}
+
+		catch(IOException e){
+			e.printStackTrace();
+		}
+
+
+	}
+
+}
---
+++ ChatServer.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.net.*;
+
+
+public ChatServer{
+	
+	public static void main(String[] args) {
+		DatagramSocket socket = null;
+		DatagramPacket packet= null;
+		byte[] buffer = null;
+
+		try {
+			socket = new DatagramSocket();
+
+			while(true){
+				String out = "first message";
+				buffer = out.getBytes();
+
+				InetAddress address = InetAddress.getByName("localhost");
+				packet = new DatagramPacket(buffer, buffer.length, address, 6789);
+
+				socket.send(packet);
+				System.out.println("Server is sending: "+ out);
+				
+			}
+		}
+
+	}
+}
